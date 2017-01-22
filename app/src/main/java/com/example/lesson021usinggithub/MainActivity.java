98f49a3fbcb4d3e7dd418df09845c828abbfa2fc
LUI PO TING <dannylui@Dannys-iMac.local>
2017-01-22 18:49:26 +0800

    Initial commit

---
+++ app/src/main/java/com/example/lesson021usinggithub/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.lesson021usinggithub;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+//collaboration
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
